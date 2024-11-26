782c94f8b9bb780ee45b316e38afbb5f22c6331a
Ilias8190 <t8190378@aueb.gr>
2024-11-26 13:34:41 +0200

    Airline.java

---
+++ Airline.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Airline {
+    private int numberOfAirplanes;
+    private int numberOfDestination;
+    private int[][] locations;
+
+   private Scanner scanner = new Scanner(System.in);
+
